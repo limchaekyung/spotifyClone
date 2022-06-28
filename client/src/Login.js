ff2ad4d8326dec7423ef194c07149fea40c04e1e
limchaekyung <corud505@naver.com>
2022-06-28 23:54:07 +0900

    spotify clone setup1

---
+++ client/src/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=4b726c687e0b4e2cbafb0f9ad6540195&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+    return (
+        <div>Login</div>
+    )
+}
