0a70f8236aeca3672e88f0bf417604cb956e89ba
limchaekyung <corud505@naver.com>
2022-06-29 23:57:34 +0900

    login

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './login';
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Container } from "react-bootstrap"
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=4b726c687e0b4e2cbafb0f9ad6540195&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
     return (
-        <div>Login</div>
+        <Container
+            className="d-flex justify-content-center align-item-center"
+            style={{ minHeight: "100vh" }}
+        >
+            <a className='btn btn-success btn-lg' href={AUTH_URL}>Login With Spotify</a>
+        </Container>
     )
 }
