c9d0fd36f5eddc4dc162835904e13790eeccade6
limchaekyung <corud505@naver.com>
2022-06-30 23:58:46 +0900

    server login

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,7 +6,7 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=4b726c687e0b4
 export default function Login() {
     return (
         <Container
-            className="d-flex justify-content-center align-item-center"
+            className="d-flex justify-content-center align-items-center"
             style={{ minHeight: "100vh" }}
         >
             <a className='btn btn-success btn-lg' href={AUTH_URL}>Login With Spotify</a>
