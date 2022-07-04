58ed95d766e5900c3c6d55ec16f02a891c2b5155
limchaekyung <corud505@naver.com>
2022-07-04 23:45:14 +0900

    search2

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import useAuth from './useAuth'
 import { Container, Form } from 'react-bootstrap'
 import SpotifyWebApi from 'spotify-web-api-node'
+import TrackSearchResult from './TrackSearchResult'
 
 const spotifyApi = new SpotifyWebApi({
     clientId: "4b726c687e0b4e2cbafb0f9ad6540195"
@@ -21,9 +22,25 @@ export default function Dashboard({ code }) {
         if (!search) return setSearchResults([])
         if (!accessToken) return
 
+        let cancel = false
+
         spotifyApi.searchTracks(search).then(res => {
-            console.log(res.body.tracks.items)
+            if (cancel) return
+            setSearchResults(res.body.tracks.items.map(track => {
+                const smallestAlbumImage = track.album.images.reduce(
+                    (smallest, image) => {
+                        if (image.height < smallest.height) return image
+                        return smallest
+                    }, track.album.images[0])
+                return {
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.uri
+                }
+            }))
         })
+        return () => cancel = true
     }, [search, accessToken])
 
     return (
@@ -37,7 +54,11 @@ export default function Dashboard({ code }) {
                 value={search}
                 onChange={e => setSearch(e.target.value)}
             />
-            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>Songs</div>
+            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+                {searchResults.map(track => (
+                    <TrackSearchResult track={track} key={track.uri} />
+                ))}
+            </div>
             <div>Bottom</div>
 
         </Container>
